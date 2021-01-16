8f5a32ed6eac89ba2ac57971c531ed66a258537d
yoganbye <67758770+yoganbye@users.noreply.github.com>
2021-01-16 17:38:23 +0400

    16.01

--- Block_chain/BC_core/views.py
+++ Block_chain/BC_core/views.py
@@ -2,24 +2,42 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Block
 from django.template import loader
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def index(request):
     """
     Главная вьюха
     """
-    block_queryset = Block.objects.all().order_by('-id')[:50]   
-    template = loader.get_template('BC_core/index.html')
+    block_queryset = Block.objects.all().order_by('-id')
+    paginator = Paginator(block_queryset, 4)
+    title = 'Главная страница'
+    
+    page = request.GET.get('page')
+    try:
+        blocks = paginator.page(page)
+    except PageNotAnInteger:        
+        # Если страница не является целым числом, поставим первую страницу 
+        blocks = paginator.page(1)
+    except EmptyPage:
+        # Если страница больше максимальной, доставить последнюю страницу результатов 
+        blocks = paginator.page(paginator.num_pages)
     context = {
-        'blocks': block_queryset
+        'blocks': blocks,
+        'page': page,
+        'title': title,
     }
-    return HttpResponse(template.render(context))
+    return render(request, "BC_core/index.html", context)
+
 
 def detail_block(request, block_id):
     """
     Вьюха детального представления
     """
-    block = Block.objects.get(id=block_id)
-    response = 'Height:{}|Hash block:{}|Time:{}\n'.format(block.id,\
-        block.hash_block, block.time) 
-    return HttpResponse(response)
+    blocks = Block.objects.get(id=block_id)
+    title = 'Детальное представление блока {}'.format(block_id)
+    context = {
+        'blocks': blocks,
+        'title': title,
+    }
+    return render(request, "BC_core/detail_block.html", context)
--- Block_chain/Block_chain/urls.py
+++ Block_chain/Block_chain/urls.py
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('BC_core.urls')),
-    path('blocks/', include('BC_core.urls')),
 ]
