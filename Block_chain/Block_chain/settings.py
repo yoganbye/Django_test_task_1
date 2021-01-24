ea2c7de338a51212540a9895e40a63a513e5c49b
yoganbye <67758770+yoganbye@users.noreply.github.com>
2021-01-24 12:43:11 +0400

    11

--- Block_chain/Block_chain/settings.py
+++ Block_chain/Block_chain/settings.py
@@ -127,8 +127,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_MEDIA = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_MEDIA = os.path.join(BASE_DIR, 'static')
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
