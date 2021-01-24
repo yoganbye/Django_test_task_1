f03b246fe71bdb217caaadd9eea388e218656f91
yoganbye <67758770+yoganbye@users.noreply.github.com>
2021-01-24 13:51:36 +0400

    sqlite3

--- Block_chain/Block_chain/settings.py
+++ Block_chain/Block_chain/settings.py
@@ -81,15 +81,15 @@ WSGI_APPLICATION = 'Block_chain.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'block_chain',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'block_chain',
+        # 'USER': 'postgres',
+        # 'PASSWORD': 'postgres',
+        # 'HOST': '127.0.0.1',
+        # 'PORT': '5432',
 
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
     }
 }
 
